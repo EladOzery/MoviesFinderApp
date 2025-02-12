3713cfb43e84e4c8635271ce9cfcda7163783393
Halel-Fruman <Halel900@gmail.com>
2025-02-12 20:06:31 +0200

    finish desigen left

--- Movie.h
+++ Movie.h
@@ -1,8 +1,14 @@
-//
-// Created by Elad on 10/02/2025.
-//
+#ifndef MOVIE_H
+#define MOVIE_H
 
-#ifndef MOVIESAPP_MOVIE_H
-#define MOVIESAPP_MOVIE_H
+#include <string>
 
-#endif //MOVIESAPP_MOVIE_H
+struct Movie {
+    std::string title;
+    std::string year;
+    std::string genre;
+    std::string imdbRating;
+    std::string posterUrl;  // Store poster URL
+};
+
+#endif // MOVIE_H
